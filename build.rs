f786c37e6da514f4aa2c26835b6ca4527c4adf3e
Pspritechologist <naaronn@gmail.com>
2024-10-25 06:52:01 -0400

    Made build script more compatible

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
 use clap::{CommandFactory, ValueEnum};
 use clap_complete::generate_to;
-use std::env::current_dir;
 use std::io::Error;
 
 include!("src/cmd/mod.rs");
 
 fn main() -> Result<(), Error> {
-	// Output into project_root/completions/
-    let out_dir = current_dir().expect("Failed to get current directory").join("completions");
+	let out_dir = match std::env::var_os("OUT_DIR") {
+		Some(out_dir) => out_dir,
+		None => return Ok(()),
+	};
 
     let mut cmd = CmdArgs::command();
 	for &shell in clap_complete::Shell::value_variants() {
